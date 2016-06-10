125201f1436f21b14ccd532535028434076c5043
Robert Cole <rcole42@gmail.com>
2016-06-10 17:42:57 -0500

    Built

--- config.rb
+++ config.rb
@@ -29,14 +29,19 @@ activate :automatic_image_sizes
 set :debug_assets, true
 
 activate :deploy do |deploy|
-  deploy.build_before = true # default: false
-  deploy.deploy_method = :git
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  deploy.branch   = 'master' # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+  deploy.method = :git
+  # remote is optional (default is "origin")
+  # run `git remote -v` to see a list of possible remotes
+  # deploy.remote = "some-other-remote-name"
+
+  # branch is optional (default is "gh-pages")
+  # run `git branch -a` to see a list of possible branches
+  deploy.branch = "master"
+
+  # strategy is optional (default is :force_push)
+  # deploy.strategy = :submodule
 end
+
 
 # --------------------------------------------------------------------------------------------------
 # Paths
